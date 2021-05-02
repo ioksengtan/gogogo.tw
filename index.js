a513495e8a6961fc095ee90e37fa7bc631e1774d
ioksengtan <ioksengtan@gmail.com>
2021-05-02 11:19:26 +0800

    20210502_1

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
+sheetsUrl = "https://docs.google.com/spreadsheets/d/1ATHYC1UnMTIhTlmnwSqaSOaRgCUgKQSYvaE6BM-5YpM/edit#gid=0";
+appUrl = "https://script.google.com/macros/s/AKfycbw3VSmnrt--uqXN07mjrlAV7nrk4Q4Ekc_hx9EN1ez7orv1Turt/exec";
 function goto(key){
-	console.log(key)
+	parameter = {
+		url: sheetsUrl,
+		command: "get_link_from_id",
+		key: key
+	};
+	$.get(appUrl, parameter, function(data) {
+		//console.log(data);
+		if(data == "na"){
+			console.log("invalid link")
+		}else{
+			window.location.href = data;
+		}
+	});
 }
